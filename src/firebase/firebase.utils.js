4174be27bd1df338482cb09006cb0aed78d3d286
aims-khan <zuhal.nader@gmail.com>
2020-11-24 19:55:51 +0430

    Add feature to firbase to store the google login user to firebase/firestore or database of firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,8 +12,30 @@ const config = {
     appId: "1:748861293984:web:8e5e287021fff3ea50d0c5",
     measurementId: "G-Q4HRE5BYXZ"
 };
-
 firebase.initializeApp(config);
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+
+    if (!userAuth) return
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+    
+    if (!snapShot.exists) {
+        const { displayName, email} = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+    return userRef;
+}
+
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore(); 
