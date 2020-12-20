ec59a3597a6d25569024e430872fada5e1ab3860
aims-khan <zuhal.nader@gmail.com>
2020-12-20 22:38:47 +0430

    Changes in shop pages

--- src/index.js
+++ src/index.js
@@ -4,17 +4,20 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom'
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import {store, persistor} from './redux/store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<PersistGate persistor={persistor}>
+					<App />
+				</PersistGate>
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,30 +1,40 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.data';
+import { Route } from 'react-router-dom';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
+// import { connect } from 'react-redux';
+// import { createStructuredSelector } from 'reselect';
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
+// import { selectCollections } from '../../redux/shop/shop.selectors';
 
-    render() {
-        const {collections} = this.state;
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({ id, ...otherCollectionProps }) => (
-                        <CollectionPreview key={id} { ...otherCollectionProps } />
-                    ))
-                }
-            </div>
-        );
-    }
-}
+// import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
+//this component is also changed from class to function just like did with directory to move the hardcoded data to redux
+
+import CollectionOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../category/collection.component';
+
+
+const ShopPage = ({ match }) => (
+
+    // const {collections} = this.state;
+
+    <div className="shop-page">
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
+        
+        {/* // collections.map(({ id, ...otherCollectionProps }) => (
+        //     <CollectionPreview key={id} { ...otherCollectionProps } />
+        // )) */}
+    </div>
+);
+
+
+
+// const mapStateToProps = createStructuredSelector({
+//     collections: selectCollections
+// })
+
 
 export default ShopPage;
